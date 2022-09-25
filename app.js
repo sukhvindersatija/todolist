00976f7d1bc527d47889ad561b5cb773529af8d7
sukhvindersatija <86351263+sukhvindersatija@users.noreply.github.com>
2022-09-25 23:54:40 +0530

    make it better

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+
 const mongoose=require("mongoose");
 const app = express();
-const _=require("lodash");
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+
 const itemSchema={
     name:String
 };
@@ -55,25 +55,7 @@ app.get("/", function(req, res) {
 
 });
 
-app.get("/:customListName",function(req,res){
-    const customListName=_.capitalize(req.params.customListName);
-    List.findOne({name:customListName},function(err,foundItems){
-      if(!err){
-        if(!foundItems){
-          const list=new List({
-            name:customListName,
-            item:[]
-          });
-          list.save();
-          res.redirect("/"+customListName);
-        }
-        else{
-          res.render("list",{listTitle: customListName, newListItems: foundItems.item});
-        }
-      }
-    })
 
-});
 
 app.post("/", function(req, res){
   const listTitle=req.body.list
@@ -81,43 +63,26 @@ app.post("/", function(req, res){
   const addItem=new Item({
     name:itemName,
   });
-  if(listTitle==="Today"){
     addItem.save();
     res.redirect("/");
-  }
-  else{
-      List.findOne({name:listTitle},function(err,foundItems){
-        foundItems.item.push(addItem);
-        foundItems.save();
-
-      });
-      res.redirect("/"+listTitle);
-
-  }
 
 });
 app.post("/delete",function(req,res){
   const listTitle=req.body.hiddenInput;
-  if(listTitle==="Today"){
-    Item.deleteOne({_id:req.body.checkbox},function(err){
-      if(err){
-        console.log("error while deleting");
-      }
+  
+    Item.deleteOne({_id:req.body.checkbox}).then(res=>{
+      console.log("deleted");
+    }).catch(err=>{
+      console.log("error while deleting");
     });
     res.redirect("/");
-  }
-  else{
-    List.findOneAndUpdate({name:listTitle},{$pull:{item:{_id:req.body.checkbox}}},function(err,foundLists){
-      if(!err){
-        res.redirect("/"+listTitle);
-      }
-
-    });
-  }
-
 });
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+mongoose.connect("mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.zbups.mongodb.net/?retryWrites=true&w=majority").then(res=>{
+  app.listen(process.env.PORT||3000).then(res=> {
+    console.log("Server started on port 3000");
+  }).catch(err=>{
+    console.log(err);
+  })
 });
+
